d678fbbdc4a7f9f3e4c841228e95a43375300ff2
unknown <alsenygueye@gmail.com>
2024-06-02 14:28:49 +0200

    fixed baseurl

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'cypress';
+// Import the defineConfig function from Cypress
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
-  projectId: 'bjtoju',
+// Export the Cypress configuration
+module.exports = defineConfig({
+  // Define configuration for end-to-end (e2e) tests
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // Set the base URL of your application
+    "baseUrl": "http://localhost:3000",
+    // Set the default viewport height for your tests
+    "viewportHeight": 1000,
+    // Set the default viewport width for your tests
+    "viewportWidth": 1280,
+    // Set the project ID for your Cypress Dashboard Service
+    projectId: "bjtoju",
   },
 });
